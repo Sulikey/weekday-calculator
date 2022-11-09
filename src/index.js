5f5733f7b3ce7b7d91046ed2cddae4bdba1f6da4
Student <student@epicodus-3.hsd1.or.comcast.net>
2022-11-09 13:52:05 -0800

    add image in css
    Co-authored-by: Robert Bryan <robbryandev@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import testImg from "./images/test.svg";
+import './js/date.js';
+import sunday from "./images/sunday.png";
+import monday from "./images/monday.jpeg";
+import tuesday from "./images/tuesday.jpeg";
+import wednesday from "./images/wednesday.png";
+import thursday from "./images/thursday.jpeg";
+import friday from "./images/friday.jpeg";
+import saturday from "./images/saturday.png";
+
+const dayImage = (day) => {
+  switch (day) {
+  case "sunday":
+    return sunday;
+  case "monday":
+    return monday;
+  case "tuesday":
+    return tuesday;
+  case "wednesday":
+    return wednesday;
+  case "thursday":
+    return thursday;
+  case "friday":
+    return friday;
+  case "saturday":
+    return saturday;
+  }
+};
 
 const getDate = () => {
   let uDate = document.getElementById("date-input").value.split("-");
   let newUDate = `${uDate[1]} ${uDate[2]} ${uDate[0]}`;
   let cd = new Date(newUDate);
-  return cd.getDay();
+  return cd.getName();
 };
 
 const handleDate = (event) => {
   event.preventDefault();
-  console.log(getDate());
+  document.getElementById("day-img").setAttribute("src", dayImage(getDate()));
+  document.getElementById("day-img").classList.remove("invisible");
 };
 
 document.querySelector("form").addEventListener("submit", handleDate);
-const myIcon = new Image();
-myIcon.src = testImg;
-
-document.getElementById("test-img").appendChild(myIcon);
-// document.getElementById("test-img").setAttribute("src", testImg);
